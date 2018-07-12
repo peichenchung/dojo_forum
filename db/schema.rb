ee1178a57c119cd525be1031088454836aa8cc55
revichung <s1492e034@gmail.com>
2018-07-12 10:14:12 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_013605) do
+ActiveRecord::Schema.define(version: 2018_07_12_021155) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_07_12_013605) do
     t.integer "user_id"
     t.integer "replies_count", default: 0
     t.integer "viewed_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
