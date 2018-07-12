04aab211ee7ec35db89026f3423877cc344a8d2d
revichung <s1492e034@gmail.com>
2018-07-12 10:16:26 +0800

    setup seed data for default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,22 @@
 
 User.create(email: "admin@example.com", password: "12345678", role: "admin")
 puts "Default admin created!"
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "旅遊" },
+  { name: "美食" },
+  { name: "電影" },
+  { name: "音樂" },
+  { name: "星座" },
+  { name: "動漫" },
+  { name: "運動" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
