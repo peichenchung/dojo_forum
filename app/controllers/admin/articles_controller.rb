3eac62ae6cf7cc237ab1b5aa76e87d7033bd59d7
revichung <s1492e034@gmail.com>
2018-07-12 10:03:07 +0800

    setup admin authentication

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ArticlesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin #自定義的方法，寫在ApplicationController中
+
   def index
     
   end
