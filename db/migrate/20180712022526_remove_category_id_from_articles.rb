5a47556816cb138e9fb2de4d064a9ae87486b21a
revichung <s1492e034@gmail.com>
2018-07-12 10:30:06 +0800

    remove category_id from articles

---
+++ db/migrate/20180712022526_remove_category_id_from_articles.rb
@@ -0,0 +1,5 @@
+class RemoveCategoryIdFromArticles < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :articles, :category_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_021155) do
+ActiveRecord::Schema.define(version: 2018_07_12_022526) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2018_07_12_021155) do
     t.integer "viewed_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
