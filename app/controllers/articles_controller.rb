6ebb936a77c93cffe007b7350451a909a0ca944b
revichung <s1492e034@gmail.com>
2018-07-14 14:19:07 +0800

    add article new and create actions and views for public users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,25 @@ class ArticlesController < ApplicationController
     @articles = Article.page(params[:page]).per(20)
     @categories = Category.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "article created"
+      redirect_to articles_path
+    else
+      flash.now[:alert] = "article failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :image, :status)
+  end
 end
