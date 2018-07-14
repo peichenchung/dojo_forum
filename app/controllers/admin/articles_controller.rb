75139cbb085c170921b695870ed9456266feecdc
revichung <s1492e034@gmail.com>
2018-07-14 13:17:23 +0800

    show categories in admin index page

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -3,6 +3,6 @@ class Admin::ArticlesController < ApplicationController
   before_action :authenticate_admin #自定義的方法，寫在ApplicationController中
 
   def index
-    
+    @categories = Category.all
   end
 end
