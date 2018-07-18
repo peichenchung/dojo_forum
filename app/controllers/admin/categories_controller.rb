f321c3ac96adf30456b2aad14caa7bb548ef85cc
revichung <s1492e034@gmail.com>
2018-07-18 18:53:01 +0800

    admin can update category with the same form on admin index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,17 +4,33 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @category = Category.new
+
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
     @category = Category.new(category_params)
     if @category.save
       flash[:notice] = "category created"
-      redirect_to admin_root_path
+      redirect_to admin_categories_path
     else
-      flash[:alert] = "category failed to create"
-      redirect_to admin_root_path
+      flash[:alert] = "category failed to create, name cannot be blank"
+      redirect_to admin_categories_path
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "category updated"
+    else
+      flash[:alert] = "category failed to update, name cannot be blank"
+      redirect_to admin_categories_path
     end
   end
 
