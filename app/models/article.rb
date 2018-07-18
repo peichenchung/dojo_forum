b9fd629b34e721fa597f7ae9af5b21df38c958f4
revichung <s1492e034@gmail.com>
2018-07-18 17:41:58 +0800

    article must assign category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates_presence_of :title
+  validates_presence_of :title, :category_ids
   has_many :article_categories
   has_many :categories, through: :article_categories
 end
