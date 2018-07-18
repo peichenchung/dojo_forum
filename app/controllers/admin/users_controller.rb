b843f2af889786f510497c853f8dacefe07d0617
revichung <s1492e034@gmail.com>
2018-07-19 06:06:14 +0800

    admin can change other user's role to admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,17 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "user role updated"
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
 end
