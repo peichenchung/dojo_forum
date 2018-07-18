1cbbbe5fdfab75d9363d3835172de63aa58692c6
revichung <s1492e034@gmail.com>
2018-07-18 19:12:33 +0800

    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:index]
 
-private
+  private
 
-def authenticate_admin
-  unless current_user.admin?
-    flash[:alert] = "Not allow!"
-    redirect_to root_path
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirect_to root_path
+    end
   end
-end
 
 end
