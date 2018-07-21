0b6213cce222355817cf14846ba3187725f9834f
revichung <s1492e034@gmail.com>
2018-07-21 13:20:32 +0800

    user can see my posts at profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :articles, dependent: :destroy
+
   has_many :comments, dependent: :destroy #使用者有很多評論
   has_many :commented_articles, through: :comments #使用者有很多評論過的文章
 
