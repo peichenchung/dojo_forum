f8f5ac430a35bbde06b825e122704a40d49eb497
revichung <s1492e034@gmail.com>
2018-07-21 14:01:20 +0800

    add validates_uniqueness to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates_presence_of :name
+  validates_uniqueness_of :name #確定新增的分類不會有重複的
   has_many :article_categories
   has_many :articles, through: :article_categories, dependent: :restrict_with_error #有關聯物件時不能刪除
 end
