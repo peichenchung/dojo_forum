9ecee82d8495bb53dc0218840bd43eef5f5dc611
revichung <s1492e034@gmail.com>
2018-07-21 11:54:19 +0800

    user can select articles by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,7 @@
 class CategoriesController < ApplicationController
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @articles = @category.articles.page(params[:page]).per(20)
+  end
 end
