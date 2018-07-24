86b889aadc665faae3ce049b9e4e21164be530e9
revichung <s1492e034@gmail.com>
2018-07-24 10:34:05 +0800

    user can edit their own comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,29 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+
+    if current_user != @comment.user
+      redirect_to article_path(@article)
+      flash[:alert] = "Not allow!!"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      flash[:notice] = "updated comment successfully"
+      redirect_to article_path(@article)
+    else
+      flash.now[:alert] = "failed to update comment"
+      render :edit
+    end
+  end
+
   def destroy
     @article = Article.find(params[:article_id])
     @comment = Comment.find(params[:id])
