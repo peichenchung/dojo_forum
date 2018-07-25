cc424c2f4f845ecc9503e19b288446dadf9908b6
revichung <s1492e034@gmail.com>
2018-07-25 19:40:04 +0800

    rails generate model collect

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_042733) do
+ActiveRecord::Schema.define(version: 2018_07_25_113720) do
 
   create_table "article_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2018_07_20_042733) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "collects", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
