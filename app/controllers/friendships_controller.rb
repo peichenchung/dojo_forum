69e87e4f3fd0250b9c86ab98d65e8212a73e9143
revichung <s1492e034@gmail.com>
2018-07-29 18:23:55 +0800

    fix friendship destroy action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,15 +1,10 @@
 class FriendshipsController < ApplicationController
   def destroy #刪除好友
-    @friendship = current_user.friendships.where(friend_id: params[:id]).first
+    @friendship = current_user.friendships.where(friend_id: params[:friend_id]).first
     @inverse_friendship = current_user.inverse_friendships.where(friend_id: current_user).first
 
-    if @friendship.present?
-      @friendship.destroy
-    end
-
-    if @inverse_friendship.present?
-      @inverse_friendship.destroy
-    end
+    @friendship.destroy
+    @inverse_friendship.destroy
 
     flash[:alert] = "Unfriend successfully!"
     redirect_back(fallback_location: root_path)
