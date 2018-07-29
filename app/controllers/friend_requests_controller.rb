fcf5e18c1ef80226e1e6e0fd624ccf62e6178c9a
revichung <s1492e034@gmail.com>
2018-07-29 16:11:17 +0800

    add 'friend' button to articles show page

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,14 @@
 class FriendRequestsController < ApplicationController
+  def create #送出好友請求
+    friend = User.find(params[:friend_id])
+    @friend_request = current_user.friend_request.new(friend: friend)
+
+    if @friend_request.save #搭配唯一性驗證
+      flash[:notice] = "You just sent a friend request"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = "You already sent a friend request before!"
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
