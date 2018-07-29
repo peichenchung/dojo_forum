228d78e4f4cb9a228a607c96813d162fba5eb4e0
revichung <s1492e034@gmail.com>
2018-07-30 01:49:31 +0800

    add default comment count to users & add name to default user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_29_132234) do
+ActiveRecord::Schema.define(version: 2018_07_29_174746) do
 
   create_table "article_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2018_07_29_132234) do
     t.string "name"
     t.text "intro"
     t.string "avatar"
-    t.integer "comments_count"
+    t.integer "comments_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Default admin
 
-User.create(email: "admin@example.com", password: "12345678", role: "admin")
+User.create(name: "admin", email: "admin@example.com", password: "12345678", role: "admin")
 puts "Default admin created!"
 
 # Category
