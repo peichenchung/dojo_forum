e05537198952b1c982ba468a13978bcb53899851
revichung <s1492e034@gmail.com>
2018-07-29 17:28:02 +0800

    add friend_request update action & show incoming friends on profile/my_friend page

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -11,4 +11,17 @@ class FriendRequestsController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
+
+  def update #接受交友請求
+    #在friendships table新增雙方的friendships記錄
+    current_user.friendships.create!(friend_id: params[:friend_id])
+    current_user.inverse_friendships.create!(user_id: params[:friend_id])
+
+    #刪除(收到的)交友請求記錄
+    @friend_request = current_user.inverse_friend_requests.where(friend_id: params[:id]).first
+    @friend_request.destroy
+
+    flash[:notice] = "You just accepted a friend request"
+    redirect_back(fallback_location: root_path)
+  end
 end
