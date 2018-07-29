c86c5abe5aabfc8deb0b56d6f5c5cae240d9fa8e
revichung <s1492e034@gmail.com>
2018-07-29 15:51:04 +0800

    setup relation between FriendRequest and User model

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,2 +1,5 @@
 class FriendRequest < ApplicationRecord
+  validates :friend_id, uniqueness: { scope: :user_id } #user不能重複送出friend_request給同一個user
+  belongs_to :user #每筆friend_request記錄,屬於送出friend_request的user
+  belongs_to :friend, class_name: "User" #每筆friend_request記錄,屬於收到friend_request的user
 end
