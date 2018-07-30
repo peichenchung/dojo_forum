eb80e06f6eca4da679bea4adb3e2ef8ab4fe9ab5
revichung <s1492e034@gmail.com>
2018-07-30 12:01:13 +0800

    add link to user_path on profile/my_friend page & authenticate user on feeds page

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,6 @@
 class FeedsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @users = User.order(comments_count: :desc).limit(10)
     @articles = Article.order(replies_count: :desc).limit(10)
