96928867eb79fc5807c57aa3308d7346fdd7bdd9
revichung <s1492e034@gmail.com>
2018-07-30 18:23:58 +0800

    remove two migration file to avoid conflict

--- db/migrate/20180717145204_add_article_and_category_to_article_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddArticleAndCategoryToArticleCategories < ActiveRecord::Migration[5.2]
-  def change
-    add_column :article_categories, :article, :belongs_to
-    add_column :article_categories, :category, :belongs_to
-  end
-end
--- db/migrate/20180717150616_remove_article_and_category_from_article_categories.rb
+++
@@ -1,6 +0,0 @@
-class RemoveArticleAndCategoryFromArticleCategories < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :article_categories, :article
-    remove_column :article_categories, :category
-  end
-end
